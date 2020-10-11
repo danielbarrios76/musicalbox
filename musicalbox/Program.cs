7db553abe112aa518c1f90cdf217093054f6d6e3
Daniel Barrios <danielbarrios76@DEV.local.com>
2020-10-11 18:41:51 -0300

    first approach

---
+++ musicalbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace musicalbox
+{
+    class Program
+    {
+      static void ShowMessage(string text, string message)
+      {
+        Console.Write(text + " " + message);
+      }
+      
+
+      static void Main(string[] args)
+      {
+        var musicalBox = new MusicalBox();
+
+        //for test
+        musicalBox.AddMusicalWord("perro");
+        musicalBox.AddMusicalWord("lado");
+        musicalBox.AddMusicalWord("casa");
+        musicalBox.AddMusicalWord("piano");
+        
+
+        ShowMessage("Palabras en la cajita musica: ", musicalBox.GetWords());
+        
+        
+
+      }
+    }
+}
